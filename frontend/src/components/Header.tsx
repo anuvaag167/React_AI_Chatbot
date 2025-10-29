53b6b0687da4fa4f7504702f301da2c90687c0a0
Anuva Agarwal <anuvaag167@gmail.com>
2025-10-29 15:55:36 -0700

    frontend started

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+
+const Header = ()=>{
+    return <>
+        <AppBar sx={{}}>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+    </>
+
+};
+
+export default Header;
